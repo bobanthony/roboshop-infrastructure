5e91d88e585b6b3bf402d8b955cdf61b24045791
Dinesh <satyadineshk328@gmail.com>
2023-03-10 22:17:14 +0530

    updating ./roboshop-infrastructure

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,7 +17,7 @@
 
 data "aws_caller_identity" "current" {}
 
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   most_recent = true
   name_regex  = "devops-practice-with-ansible"
   owners      = [data.aws_caller_identity.current.account_id]

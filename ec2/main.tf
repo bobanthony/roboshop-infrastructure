a5af5cc53d19d0d8ecbd675c8e8cb169035af597
Dinesh <satyadineshk328@gmail.com>
2023-03-10 14:59:10 +0530

    updating ./roboshop-infrastructure

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.component]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     name = "var.component"
   }

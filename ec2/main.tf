3d76f6d505a0806d1eda005e526bd2885e65d303
Dinesh <satyadineshk328@gmail.com>
2023-03-10 21:10:27 +0530

    updating ./roboshop-infrastructure

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,4 @@
+//noinspection HILUnresolvedReference
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = var.instance_type
@@ -61,4 +62,5 @@ variable "component" {}
 variable "instance_type" {}
 
 variable "env" {}
+
 

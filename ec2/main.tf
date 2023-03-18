eb904152bd2d0bb80e55be718d43747e4f0a035a
Dinesh <satyadineshk328@gmail.com>
2023-03-18 17:49:38 +0530

    updating ./roboshop-infrastructure

--- ec2/main.tf
+++ ec2/main.tf
@@ -42,6 +42,7 @@ resource "aws_instance" "ec2" {
   iam_instance_profile   = "${var.env}-${var.component}-role"
   tags = {
     Name = var.component
+    monitor = var.monitor ? "yes" : "no"
   }
 }
 resource "null_resource" "provisioner" {
